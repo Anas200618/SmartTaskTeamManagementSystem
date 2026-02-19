06e8c87693c590eb3eb5bf7a4a9b718f341bb95b
Anas Mansuri <127174811+Anas200618@users.noreply.github.com>
2026-02-19 14:31:42 +0530

    Use environment variable for socket URL

--- client/src/components/NotificationDropdown.jsx
+++ client/src/components/NotificationDropdown.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import API from "../services/api";
 
-const socket = io("https://smarttaskteammanagementsystem.onrender.com"); // Your Backend URL
+const socket = io(process.env.REACT_APP_API_URL); // Your Backend URL
 
 const NotificationDropdown = ({ currentTheme }) => {
   const [notifications, setNotifications] = useState([]);
